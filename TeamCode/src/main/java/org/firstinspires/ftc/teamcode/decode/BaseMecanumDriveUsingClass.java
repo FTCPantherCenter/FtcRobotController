c6967d875b28331f15994d6704cc385161d5ebb3
Luprix <dhillonarjun2012@gmail.com>
2025-11-06 17:12:41 -0600

    Small changes and updates(typos, etc)
    Fixed some typos and code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/BaseMecanumDriveUsingClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/BaseMecanumDriveUsingClass.java
@@ -6,14 +6,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
- * This is a teleop implictation of the basiFunctions class
+ * This is a teleop implication of the basicFunctions class
  * We begin by declaring but not defining basic functions out of any methods, then define basic functions, named do Stuff here, in the init method
  * We also run the doStuff init method with the HardwareMap from the OpMode init method
- * The loop integrates the turn and move methods, with the left stick x controlling turning, and the right stick x and y controlling straffing
+ * The loop integrates the turn and move methods, with the left stick x controlling turning, and the right stick x and y controlling staffing
  * The move method in doStuff is used to stop the robot
  */
 
-@TeleOp
+@TeleOp(name = "Base Mecanum Drive Using Class", group = "Drive")
 public class BaseMecanumDriveUsingClass extends OpMode {
 basicFunctions doStuff;
 
@@ -34,7 +34,7 @@ public void init() {
 
 
         if (gamepad1.right_stick_x != 0.0 || gamepad1.right_stick_y != 0.0) {
-            doStuff.move(gamepad1.right_stick_x,gamepad1.right_stick_y);
+            doStuff.move(gamepad1.right_stick_y,gamepad1.right_stick_x);
         }
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/TestClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/TestClass.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-@Disabled
+
 public class TestClass extends OpMode {
 
     @Override
